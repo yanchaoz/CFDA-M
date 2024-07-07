b14b91e0fef63ca05b6ddb596b21450c19ce1ffd
Zhangyc <47974713+yanchaoz@users.noreply.github.com>
2024-07-07 20:42:09 +0800

    Add files via upload

---
+++ stage1/scripts/train_cut2seg_VNC2Lucchi.sh
@@ -0,0 +1,25 @@
+CUDA_VISIBLE_DEVICES=0 python train.py \
+--name experiment_cut2seg \
+--raw_A_dir ./preprocess/VNC2Lucchi/VNC/ \
+--raw_A_seg_dir ./preprocess/VNC2Lucchi/VNC/ \
+--raw_B_dir ./preprocess/VNC2Lucchi/Lucchi/ \
+--sub_list_A ./preprocess/VNC2Lucchi/train_VNC.txt \
+--sub_list_B ./preprocess/VNC2Lucchi/train_Lucchi.txt \
+--batch_size 4 \
+--angle 0 \
+--model cut2seg_model_train \
+--netG resnet_9blocks \
+--netD basic \
+--netS duseunet \
+--pool_size 50 \
+--no_dropout \
+--dataset_mode cut2seg_train \
+--input_nc 1  \
+--output_nc 1 \
+--output_nc_seg 1 \
+--lambda_GAN 1.0 \
+--lambda_NCE 1.0 \
+--lambda_DICE 1.0 \
+--lambda_SC 0.0 \
+--checkpoints_dir ./checkpoints/VNC2Lucchi/ \
+--display_id 0
